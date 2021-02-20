dfcb22a72d42c352d57cbd072b97feaa2afca383
andresdev97 <andrescardosodev.97@gmail.com>
2021-02-20 16:57:09 -0300

    Updates

---
+++ precache-manifest.6d9ec8e7a81a920e99364b19d7707d9b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3c10fdb95b235e3ecf4c0b0ad1db30",
+    "url": "/practica-alpha-tauri/index.html"
+  },
+  {
+    "revision": "90689779d7c3fad013f2",
+    "url": "/practica-alpha-tauri/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "f23924c6c976e1dfe110",
+    "url": "/practica-alpha-tauri/static/css/main.5f2fe40e.chunk.css"
+  },
+  {
+    "revision": "90689779d7c3fad013f2",
+    "url": "/practica-alpha-tauri/static/js/2.b281a6f9.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/practica-alpha-tauri/static/js/2.b281a6f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23924c6c976e1dfe110",
+    "url": "/practica-alpha-tauri/static/js/main.6623a14a.chunk.js"
+  },
+  {
+    "revision": "ab55ffc146684b26834c",
+    "url": "/practica-alpha-tauri/static/js/runtime-main.c2a5f7e8.js"
+  },
+  {
+    "revision": "9bd56672335793998bb6c94810b97636",
+    "url": "/practica-alpha-tauri/static/media/alpha-carousel1.9bd56672.jpg"
+  },
+  {
+    "revision": "f91cd9cd644371d5afd24539c6c96806",
+    "url": "/practica-alpha-tauri/static/media/alpha-carousel2.f91cd9cd.jpg"
+  },
+  {
+    "revision": "933f8e75a96f45ecc63ff0ddf2a54eb2",
+    "url": "/practica-alpha-tauri/static/media/alpha-carousel3.933f8e75.jpg"
+  },
+  {
+    "revision": "f8683c8a09e0ce362a161052cd9748e2",
+    "url": "/practica-alpha-tauri/static/media/alpha-tauri.f8683c8a.png"
+  }
+]);
